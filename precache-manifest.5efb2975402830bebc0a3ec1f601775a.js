cdfc5dc7d92a290f52a12ebfa188daee1ad1293d
CI System <sh@signalwerk.ch>
2019-09-09 00:45:07 +0000

    Deploy to GitHub Pages: 33d3c76b31790a77014408db2d207dabb49349f3

---
+++ precache-manifest.5efb2975402830bebc0a3ec1f601775a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a92b8f583facfdf7436800a60ada1cbc",
+    "url": "/calendar/index.html"
+  },
+  {
+    "revision": "285cfcf99a8e3cdd48ef",
+    "url": "/calendar/static/css/main.bbcc614f.chunk.css"
+  },
+  {
+    "revision": "d72f2bd27a07bbf37a8d",
+    "url": "/calendar/static/js/2.fa5a19e2.chunk.js"
+  },
+  {
+    "revision": "285cfcf99a8e3cdd48ef",
+    "url": "/calendar/static/js/main.499f058a.chunk.js"
+  },
+  {
+    "revision": "3a0231add6fe18032911",
+    "url": "/calendar/static/js/runtime~main.7f9d13b3.js"
+  }
+]);
