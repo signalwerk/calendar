80814946812d1ae786771d9f44e3a7b7409d52d0
CI System <sh@signalwerk.ch>
2020-01-03 16:28:04 +0000

    Deploy to GitHub Pages: b10c2a9e55002656870e96ac96f44d616f98a340

---
+++ precache-manifest.85210059c5fd4dd8157c480a88c425c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38802115a9b75b7b9f1ab6c96abc1795",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7bcfc79381b957fd0d8",
+    "url": "/static/css/main.316d9b28.chunk.css"
+  },
+  {
+    "revision": "ded8feb87054391ec801",
+    "url": "/static/js/2.f366a7f6.chunk.js"
+  },
+  {
+    "revision": "a7bcfc79381b957fd0d8",
+    "url": "/static/js/main.69f0ade6.chunk.js"
+  },
+  {
+    "revision": "328e9b983497804a1ee8",
+    "url": "/static/js/runtime~main.1b3b4be2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/calendar/precache-manifest.74f2374c5844f8aff8e1bba58b2f6f60.js"
+  "/precache-manifest.85210059c5fd4dd8157c480a88c425c1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/calendar/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
